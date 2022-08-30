6a720e7b40b0cb40df1d7caaf6d06e0f42fb21d9
puun555 <105053170+puun555@users.noreply.github.com>
2022-08-31 00:35:57 +0700

    lab04

---
+++ WEEK4/Vehicle.java
@@ -0,0 +1,25 @@
+public class Vehicle {
+    private int fuel;
+    private String topSpeed;
+
+    protected void setFuel(int i) {
+        if ( i >= 0) {fuel = i;}
+        else {fuel += i;}
+    }
+
+    protected void setTopSpeed(String n) {
+        topSpeed = n;
+    }
+
+    protected int getFuel() {
+        return fuel;
+    }
+
+    protected String getTopSpeed() {
+        return topSpeed;
+    }
+
+    public void showInfo() {
+        System.out.println("Fuel is "+ fuel + " litre and Top Speed is " + topSpeed + " m/s.");
+    }
+}
