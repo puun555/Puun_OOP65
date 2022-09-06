4178e5c5c220ad147eb7fa87815553b62e799d45
puun555 <105053170+puun555@users.noreply.github.com>
2022-09-07 00:44:43 +0700

    sssssdsd
    sdsds

---
+++ WEEK5/Customer.java
@@ -0,0 +1,54 @@
+package WEEK5;
+
+public class Customer extends CheckingAccount{
+    private String firstName;
+    private String lastname;
+    private CheckingAccount acct;
+
+    public Customer() {
+        this("", "", null);
+    }
+    public Customer(String firstName, String lastName) {
+        this(firstName, lastName, null);
+    }
+    public Customer(String firstName, String lastName, CheckingAccount acct) {
+        this.firstName = firstName;
+        this.lastname = lastName;
+        this.acct = acct;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setAcct(CheckingAccount acct) {
+        this.acct = acct;
+    }
+    public CheckingAccount getAcct() {
+        return acct;
+    }
+
+    public String toString() {
+        if (acct == null) {return firstName + " " + lastname + " doesn't have account.";}
+        else {return "The " + firstName + " account has " + acct.balance + " baht and " + acct.getCredit() + " credits.";}
+        
+    }
+
+
+    public boolean equals(Customer c) {
+        if (this.lastname.equals(c.lastname)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
--- WEEK5/Main.java
+++ WEEK5/Main.java
@@ -2,11 +2,11 @@ package WEEK5;
 
 public class Main {
     public static void main(String[] args) {
-        // (1)Dont have default Constructor
+        // (1.1)Dont have default Constructor
         // Account a1 = new Account();
         // a1.showAccount();
 
-        // (1)nice!!
+        // (1.2)nice!!
         // Account a1 = new Account(50000,"61070033");
         // a1.showAccount();
         // a1.deposit(500);
@@ -14,7 +14,7 @@ public class Main {
         // a1.withdraw(40000);
         // a1.showAccount();
 
-        //(2) OKOK
+        //(2.1) OKOK
         // CheckingAccount a1 = new CheckingAccount(50000,"61070033",5000);
         // a1.showAccount();
         // a1.deposit(500);
@@ -22,15 +22,42 @@ public class Main {
         // a1.withdraw("40000.0");
         // System.out.println(a1.toString());
 
-        //(2) nice!!
+        //(2.2) nice!!
         // CheckingAccount a1 = new CheckingAccount();
         // a1.deposit(500);
         // System.out.println(a1);
         // a1.withdraw(40000.0);
         // System.out.println(a1.toString());
 
-        Account a1 = new CheckingAccount();
-        a1.setCredit(1000);
-        System.out.println(a1);
+
+        //(2.3)With Solution
+        // Account a1 = new CheckingAccount();
+        //  a1.setCredit(1000); error แปะป้ายผิด
+        // ((CheckingAccount)a1).setCredit(1000);
+        // System.out.println(a1);
+
+        //(2.4)
+        // Account a1 = new CheckingAccount();
+        // a1.setName("Nook");
+        // System.out.println(a1);
+        // ((CheckingAccount)a1).setCredit(1000);
+        // System.out.println(a1);
+
+        //(3.1)
+        // CheckingAccount a1 = new CheckingAccount(1000,"62070033",500);
+        // Customer c1 = new Customer();
+        // Customer c2 = new Customer("Harry","Potter");
+        // Customer c3 = new Customer("Harry","Potter",a1);
+        // System.out.println(c1);
+        // System.out.println(c2);
+        // System.out.println(c3);
+
+        //(3.2)
+        // CheckingAccount a1 = new CheckingAccount(1000,"62070033",500);
+        // Customer c1 = new Customer();
+        // Customer c2 = new Customer("Harry","Potter");
+        // Customer c3 = new Customer("Harry","Potter",a1);
+        // System.out.println(c1.equals(c2));
+        // System.out.println(c3.equals(c2));
     }
 }
