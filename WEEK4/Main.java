64f37031bb254445c46e41fc4dc88c601a71ef65
PuunKotHodJumpFuc <105053170+puun555@users.noreply.github.com>
2022-09-07 10:50:19 +0700

    pppp
    sss

--- WEEK4/Main.java
+++ WEEK4/Main.java
@@ -1,11 +1,21 @@
 public class Main {
+
     public static void main(String[] args) {
-    Plane p1 = new Plane();
-    p1.setPlaneInfo(300, "High");
-    p1.showPlaneInfo();
-    p1.fly();
-    p1.showPlaneInfo();
-    p1.fly();
-    p1.showPlaneInfo();
+  
+      Car c1 = new Car();
+  
+      c1.setCarInfo(60, "High", "Diesel");
+  
+      c1.showCarInfo();
+  
+      c1.move();
+  
+      c1.showCarInfo();
+  
+      c1.move();
+  
+      c1.showCarInfo();
+  
     }
-}
+  
+  }
