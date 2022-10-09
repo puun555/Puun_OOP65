20d77a79b5d6fb80106d841db4fa2effb0bbd7f5
puun555 <105053170+puun555@users.noreply.github.com>
2022-10-09 13:12:13 +0700

    s

---
+++ WEEK7/test/Main.java
@@ -0,0 +1,13 @@
+package WEEK7.test;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Title");
+
+        f.setSize(200, 200);
+
+        
+    }
+}
---
+++ WEEK7/test/Test.java
@@ -0,0 +1,8 @@
+package WEEK7.test;
+import java.awt.*;
+import javax.swing.*;;
+
+public class Test extends JFrame{
+    private JFrame f;
+    private JMenuBar mb;
+}
