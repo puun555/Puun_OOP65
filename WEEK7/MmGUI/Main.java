b90d3f37bf4651c6eeec42e01660031ff5cb2e62
PuunKotHodJumpFuc <105053170+puun555@users.noreply.github.com>
2022-10-19 14:06:18 +0700

    c
    x

--- WEEK7/MmGUI/Main.java
+++ WEEK7/MmGUI/Main.java
@@ -1,7 +1,13 @@
 package WEEK7.MmGUI;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-    new MnGUI();
+        try { UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+          e.printStackTrace();
+    }
+    SwingUtilities.invokeLater(() -> { MnGUI frame = new MnGUI(); });
     }
 }
